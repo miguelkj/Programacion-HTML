fd33f52bdc92d8d9c39a832a994274ea83f8aa76
miguelkj <49109118+miguelkj@users.noreply.github.com>
2019-03-31 22:55:00 -0400

    hola

---
+++ HTML helpers/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTML_helpers.Models;
+
+namespace HTML_helpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+
+
+        public ActionResult MostarD( Datos datos )
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(datos);
+            }
+            return View("MostarD", datos);
+        }
+
+
+    }
+}
---
+++ HTML helpers/Models/Datos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HTML_helpers.Models
+{
+    public class Datos
+    {
+        
+
+        [Required(ErrorMessage = "Ingresar cedula")]
+        public int Cedula { get; set; }
+
+
+
+        [Required(ErrorMessage = "Ingresar nombre")]
+        public string Nombre { get; set; }
+
+
+
+        [Required(ErrorMessage = "Ingresar apellido")]
+        public string Apellido { get; set; }
+        
+        [Range(15,100)]
+        public int Edad { get; set; }
+
+        [Required(ErrorMessage = "Ingresa telefono")]
+        public int Telefono { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "ingresa el correo")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Correo no valido.")]
+
+        public string Correo { get; set; }
+
+        public string Genero { get; set; }
+
+        public string EstadoCivil { get; set; }
+
+        public string Hobbys { get; set; }
+    }
+}
